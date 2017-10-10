6249475fbf0da6425830d6a67a5bec8d152d8cc6
Vladimir Alefirov <31669425+Alifa00@users.noreply.github.com>
2017-10-10 22:35:59 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+
+{   int n=0;
+    double* P=(double*)malloc(sizeof(double));
+    char* command=(char*)malloc(sizeof(char));
+    string str;
+    double b;
+    char com;
+    getline(cin, str);
+    istringstream stream(str);
+    stream >> *P;
+    while (stream >> com)
+    {switch(com) {
+        case '+':
+            if(stream >> b){
+                command=(char*)realloc(command,(n+1)*sizeof(char));
+                *(command+n)=com;
+            }
+            
+            n++;
+            P=(double*)realloc(P,(n+1)*sizeof(double));
+            *(P+n)=b;
+            break;
+        case '-':
+            if(stream >> b){
+                command=(char*)realloc(command,(n+1)*sizeof(char));
+                *(command+n)=com;
+            }
+            n++;
+            P=(double*)realloc(P,(n+1)*sizeof(double));
+            *(P+n)=b;
+            break;
+        case '*':
+            stream >> b;
+            *(P+n) = (*(P+n)) * b;
+            
+            
+            break;
+        case '/':
+            stream >> b;
+            *(P+n) = (*(P+n)) /b;
+            break;
+    }
+    }
+    double rs=*P;
+    for(int i=0;i<n+1;i++){
+        switch(*(command+i)){
+            case '+':
+                rs+=*(P+i+1); break;
+            case '-':
+                rs-=*(P+i+1); break;
+                
+        }
+    }
+    
+    cout << rs;
+    return 0;
+    
+}
